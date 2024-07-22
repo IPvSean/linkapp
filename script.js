a2ab63f870354684f2414c320db5f2ad9437e187
sean cavanaugh <sean@cavanaugh.pro>
2024-07-22 16:18:29 -0400

    sync

--- script.js
+++ script.js
@@ -4,9 +4,21 @@ function generateLinks() {
     const linkedinCode = '7015Y0000048LatQAE';
     const instagramCode = '7015Y0000048Lb3QAE';
     const tiktokCode = '7015Y0000048Lb8QAE';
+    const ansibleTwitterCode = '7013a000003DTdoAAG';
+    const ansibleYoutubeCode = '7015Y000003t3zHQAQ';
 
-    document.getElementById('twitterLink').textContent = `${url}?sc_cid=${twitterCode}`;
-    document.getElementById('linkedinLink').textContent = `${url}?sc_cid=${linkedinCode}`;
-    document.getElementById('instagramLink').textContent = `${url}?sc_cid=${instagramCode}`;
-    document.getElementById('tiktokLink').textContent = `${url}?sc_cid=${tiktokCode}`;
+    document.getElementById('twitterLink').value = `${url}?sc_cid=${twitterCode}`;
+    document.getElementById('linkedinLink').value = `${url}?sc_cid=${linkedinCode}`;
+    document.getElementById('instagramLink').value = `${url}?sc_cid=${instagramCode}`;
+    document.getElementById('tiktokLink').value = `${url}?sc_cid=${tiktokCode}`;
+    document.getElementById('ansibleTwitterLink').value = `${url}?sc_cid=${ansibleTwitterCode}`;
+    document.getElementById('ansibleYoutubeLink').value = `${url}?sc_cid=${ansibleYoutubeCode}`;
+}
+
+function copyToClipboard(elementId) {
+    const copyText = document.getElementById(elementId);
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+    document.execCommand("copy");
+    alert("Copied the URL: " + copyText.value);
 }
